61512ab9b5166179740c32e2a7ea4300d0fa83b9
Ксения <easyghoul@gmail.com>
2024-02-16 15:26:12 +0700

    feat: Добавляет url-адреса и ключ API

---
+++ script.js
@@ -0,0 +1,3 @@
+const API_KEY = '0fe8718ffa0095832e20cc9e';
+const BASE_URL = 'https://v6.exchangerate-api.com/v6';
+const DATA_URL = 'https://restcountries.com/v3.1/all?fields=name,currencies,flag';
